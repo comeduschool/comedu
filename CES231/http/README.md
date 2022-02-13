ab790eb6afa81e0a534746267a1191fa7fc7bde9
imyme6yo <imyme6yo@gmail.com>
2022-02-13 19:24:58 +0900

    CES231: HTTP Example

--- CES231/http/README.md
+++ CES231/http/README.md
@@ -4,7 +4,7 @@ Dockerfile이 있는 폴더에서 아래를 실행합니다.
 (필수 `app01.js` 파일과 `app02.js` 파일도 같은 위치에 있어야 합니다.
 
 ```shell
-docker build -t http:example .
+docker build -t http:test .
 ```
 
 ## 컨테이너 실행
@@ -12,16 +12,16 @@ docker build -t http:example .
 - 예제 1번 실행
 
 ```shell
-docker run -d -p 3100:3000 --name http-ex01 http:example app01.js
+docker run -d -p 3100:3000 --name http-01 http:test node app01.js
 ```
 
 - 예제 2번 실행
 
 ```shell
-docker run -d -p 3101:3000 --name http-ex02 http:example app02.js
+docker run -d -p 3101:3000 --name http-02 http:test node app02.js
 ```
 
 ## 요청 보내기
 ```shell
-
+nc localhost 3102 < req.txt
 ```
--- CES231/http/app02.js
+++ CES231/http/app02.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
+  console.log("/")
   res.send('Hello Comedu!')
 })
 
@@ -15,13 +16,17 @@ Content-Length: 48
 `
 
 app.get('/raw', (req, res) => {
+  console.log("/raw")
   res.socket.end(responseMessage)
 })
 
 app.post('/raw', (req, res)=>{
+  console.log("/raw")
+  console.log(`headers: ${req.headers}`)
+  console.log(`body: ${req.body}`)
   const data = {
-    'headers': JSON.parse(JSON.stringify(req.headers)),
-    'body': JSON.parse(JSON.stringify(req.body))
+    'headers': JSON.stringify(req.headers),
+    'body': JSON.stringify(req.body)
   };
   res.send(data);
 })
